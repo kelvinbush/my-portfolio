9a92ddad852ec287ba22c58cd74c8bb275aa2f4f
Kelvin Bush <kelybush@gmail.com>
2022-02-23 18:15:38 +0300

    fix date bug
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- portfolio_frontend/src/container/About/About.jsx
+++ portfolio_frontend/src/container/About/About.jsx
@@ -1,31 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./About.scss";
 import { motion } from "framer-motion";
-import images from "../../contants";
+import { client, urlFor } from "../../client";
 
 const About = () => {
-  const abouts = [
-    {
-      title: "Front-End Development",
-      description: "I am a good web developer",
-      imgUrl: images.about01,
-    },
-    {
-      title: "Android Development",
-      description: "I am a good web developer",
-      imgUrl: images.about02,
-    },
-    {
-      title: "Back-End Development",
-      description: "I am a good web developer",
-      imgUrl: images.about03,
-    },
-    {
-      title: "Blockchain Development",
-      description: "I am a good web developer",
-      imgUrl: images.about04,
-    },
-  ];
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client
+      .fetch(query)
+      .then((data) => setAbouts(data))
+      .catch((e) => console.log(e));
+  }, []);
 
   return (
     <>
@@ -43,7 +29,7 @@ const About = () => {
             className={"app__profile-item"}
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
