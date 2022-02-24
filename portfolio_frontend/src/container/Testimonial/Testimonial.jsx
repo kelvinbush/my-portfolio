1f9717207067cdef691a9708d8ae99381c79db80
Kelvin Bush <kelybush@gmail.com>
2022-02-24 13:44:23 +0300

    testimonials complete
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- portfolio_frontend/src/container/Testimonial/Testimonial.jsx
+++ portfolio_frontend/src/container/Testimonial/Testimonial.jsx
@@ -3,6 +3,8 @@ import "./Testimonial.scss";
 import { motion } from "framer-motion";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import { client, urlFor } from "../../client";
+import AppWrap from "../../wrapper/AppWrap";
+import MotionWrap from "../../wrapper/Motion";
 
 const Testimonial = () => {
   const [brands, setBrands] = useState([]);
@@ -26,8 +28,71 @@ const Testimonial = () => {
       })
       .catch((e) => console.log(e));
   }, []);
+  const test = testimonials[currentIndex];
 
-  return <div>Testimonial</div>;
+  function handleClick(index) {
+    setCurrentIndex(index);
+  }
+
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img src={urlFor(test.imgurl)} alt={"testimonial"} />
+            <div className="app__testimonial-content">
+              <p className="p-text">{test.feedback}</p>
+              <div>
+                <h4 className="bold-text">{test.name}</h4>
+                <h5 className="bold-text">{test.company}</h5>
+              </div>
+            </div>
+          </div>
+          <div className="app__testimonial-btns app__flex">
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  currentIndex === 0
+                    ? testimonials.length - 1
+                    : currentIndex - 1
+                )
+              }
+            >
+              <HiChevronLeft />
+            </div>
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  currentIndex === testimonials.length - 1
+                    ? 0
+                    : currentIndex + 1
+                )
+              }
+            >
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+      <div className="app__testimonials-brands app__flex">
+        {brands.map((brand) => (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: "tween" }}
+            key={brand._id}
+          >
+            <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default Testimonial;
+export default AppWrap(
+  MotionWrap(Testimonial, "app__testimonial"),
+  "testimonial",
+  "app__primarybg"
+);
