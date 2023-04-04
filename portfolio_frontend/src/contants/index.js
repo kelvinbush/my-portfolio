f4e3d62587b30dc0a8025652beb19d1f6425de44
Kelvin Wachiye <kelybush@gmail.com>
2023-04-04 15:08:11 +0300

    update profile

--- portfolio_frontend/src/contants/index.js
+++ portfolio_frontend/src/contants/index.js
@@ -36,7 +36,7 @@ import bolt from "../assets/bolt.png";
 import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
-import me from "../assets/me.png";
+import me from "../assets/me.jpg";
 import kelvin_logo from "../assets/kelvin_logo.svg";
 import android_jetpack from "../assets/android-jetpack.svg";
 import android_robot from "../assets/android_robot.svg";
